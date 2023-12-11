369789e97bd5c52c85bd0a6e9b2466cb35d578a5
ernestbone <owhiroroernest@gmail.com>
2023-12-11 12:23:20 +0100

    display products on page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,21 @@ function App() {
   return (
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                className="product-image"
+                src={product.image}
+                alt={product.name}
+              ></img>
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All Rights Reserved</footer>
     </div>
   )
