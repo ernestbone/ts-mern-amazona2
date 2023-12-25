57604729c4c2fcb12f5833987e3581e410cfd683
ernestbone <owhiroroernest@gmail.com>
2023-12-25 15:46:47 +0100

    implementerd add to cart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { useContext, useEffect } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { LinkContainer } from 'react-router-bootstrap'
 
-import { Container, Navbar, Nav, Button } from 'react-bootstrap'
+import { Container, Navbar, Nav, Button, Badge } from 'react-bootstrap'
 
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import { Store } from './Store'
 
 function App() {
   const {
-    state: { mode },
+    state: { mode, cart },
     dispatch,
   } = useContext(Store)
 
@@ -21,18 +24,26 @@ function App() {
 
   return (
     <div className="d-flex flex-column vh-100">
+      <ToastContainer position="bottom-center" limit={1} />
       <header>
         <Navbar expand="lg">
           <Container>
-            <Navbar.Brand>TsAmazona</Navbar.Brand>
+            <LinkContainer to="/">
+              <Navbar.Brand>TsAmazona</Navbar.Brand>
+            </LinkContainer>
           </Container>
           <Nav>
             <Button variant={mode} onClick={switchModeHandler}>
               <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
             </Button>
-            <a href="/cart" className="nav-link">
-              cart
-            </a>
+            <Link to="/cart" className="nav-link">
+              Cart
+              {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}
+            </Link>
             <a href="/signin" className="nav-link">
               Sign in
             </a>
--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -2,8 +2,31 @@ import { Button, Card } from 'react-bootstrap'
 import { Product } from '../types/Product'
 import { Link } from 'react-router-dom'
 import Rating from './Rating'
+import { useContext } from 'react'
+import { Store } from '../Store'
+import { CartItem } from '../types/Carts'
+import { convertProductToCartItem } from '../utils'
+import { toast } from 'react-toastify'
 
 function ProductItem({ product }: { product: Product }) {
+  const { state, dispatch: dispatch } = useContext(Store)
+  const {
+    cart: { cartItems },
+  } = state
+
+  const addToCartHandler = async (item: CartItem) => {
+    const existItem = cartItems.find((x) => x._id === product._id)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock')
+      return
+    }
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    })
+    toast.success('Product added to the cart')
+  }
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -20,7 +43,11 @@ function ProductItem({ product }: { product: Product }) {
             Out of stock
           </Button>
         ) : (
-          <Button>Add to cart</Button>
+          <Button
+            onClick={() => addToCartHandler(convertProductToCartItem(product))}
+          >
+            Add to cart
+          </Button>
         )}
       </Card.Body>
     </Card>
